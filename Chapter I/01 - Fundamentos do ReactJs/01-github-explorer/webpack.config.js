7405b23e114e9c6bcd857ddd47c2953f9df688b5
Bruno Milano <brn.milano@hotmail.com>
2023-04-20 12:23:25 -0300

    Chapter I - 01 - Configurando a API - Aulas 04 a 05

---
+++ Chapter I/01 - Fundamentos do ReactJs/01-github-explorer/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+// Coloca a barra / de acordo com o sistema operacional
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ],
+  }
+};
