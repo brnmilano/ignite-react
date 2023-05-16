f42e4368c58e3ce1ee0a0a6bdbab31969aa6a710
Bruno Milano <brn.milano@hotmail.com>
2023-05-16 15:35:32 -0300

    Chapter III - NextJs - 02 - Componentes e páginas - Aulas 01 a 04

--- Chapter III - NextJs/02 - Componentes e páginas/ignews/src/app/page.tsx
+++ Chapter III - NextJs/02 - Componentes e páginas/ignews/src/app/page.tsx
@@ -1,9 +1,30 @@
-import styles from '../styles/home.module.scss'
+import { Header } from '@/components/Header'
+import styles from './page.module.scss'
+import { SubscribeButton } from '@/components/SubscribeButton'
 
 export default function Page() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>
+            👏 Hey, wellcome
+          </span>
+
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="images/avatar.svg" alt="Girl Coding" />
+      </main>
     </div>
   )
 }
